8fa8ac23aa742c72e3eeaec0a4bef2186e2bed11
Randall Brenes <rjb_mcquiddy@hotmail.com>
2024-10-08 13:09:32 -0600

    yarn lint fix

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -27,7 +27,7 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
-  }
+  },
 ];
 
 export const HeaderMenuLinks = () => {
--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     JWRToken: {
-      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
       abi: [
         {
           inputs: [
@@ -246,7 +246,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     StakingContract: {
-      address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+      address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
       abi: [
         {
           inputs: [
